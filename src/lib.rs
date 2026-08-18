e9ed4ff3f5943b503b2baf935311cdf745760771
Rohit <rohitnarnolia88@gmail.com>
2026-08-18 19:48:32 +0530

    chore: add structure for runtime

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub mod runtime;
 pub mod error;
-pub mod task;
 pub mod reactor;
+pub mod runtime;
+pub mod task;
--- src/runtime.rs
+++ src/runtime.rs
@@ -1,16 +1,60 @@
 #![allow(dead_code)]
 
-use crate::error;
+use std::{
+    collections::BTreeMap,
+    sync::{Arc, Mutex},
+    task::Waker,
+    time::Instant,
+};
 
+use crate::{
+    error,
+    task::{DynFuture, JoinHandle, JoinState, wrap_with_join_state},
+};
 
-#[derive(Debug)]
+struct Shared {
+    new_tasks: Mutex<Vec<DynFuture>>,
+    wake_times: Mutex<BTreeMap<Instant, Vec<Waker>>>,
+}
+
+impl Shared {
+    pub fn new() -> Self {
+        Self {
+            new_tasks: Mutex::new(Vec::new()),
+            wake_times: Mutex::new(BTreeMap::new()),
+        }
+    }
+}
+
 pub struct Runtime {
-    _priv: (),
+    shared: Arc<Shared>,
 }
 
 impl Runtime {
     pub fn new() -> error::Result<Self> {
-        Ok(Self { _priv: () })
+        Ok(Self {
+            shared: Arc::new(Shared::new()),
+        })
+    }
+
+    pub fn spawn<F, T>(&self, future: F) -> JoinHandle<T>
+    where
+        F: Future<Output = T> + Send + 'static,
+        T: Send,
+    {
+        let join_state = Arc::new(Mutex::new(JoinState::Unawaited));
+
+        let join_handle = JoinHandle {
+            state: Arc::clone(&join_state),
+        };
+
+        self.shared
+            .new_tasks
+            .lock()
+            .unwrap()
+            .push(Box::pin(wrap_with_join_state(future, join_state)));
+
+        join_handle
     }
 
     pub fn block_on<F: Future>(&self, _fut: F) -> F::Output {
