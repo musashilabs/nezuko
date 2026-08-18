53b9112447c4b5d07a7aef7a8ad6e3894c88ab35
Rohit <rohitnarnolia88@gmail.com>
2026-08-18 21:50:44 +0530

    chore: add sleep from practice repo

---
+++ src/time/sleep.rs
@@ -0,0 +1,28 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+    time::{Duration, Instant},
+};
+
+pub struct Sleep {
+    wake_time: Instant,
+}
+
+pub fn sleep(duration: Duration) -> Sleep {
+    Sleep {
+        wake_time: Instant::now() + duration,
+    }
+}
+
+impl Future for Sleep {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, context: &mut Context) -> Poll<()> {
+        if Instant::now() >= self.wake_time {
+            Poll::Ready(())
+        } else {
+            crate::runtime::register_sleep(self.wake_time, context.waker().clone());
+            Poll::Pending
+        }
+    }
+}
