0fe68bf0c5eea8e104bf66d2cff69e2ee35979ae
Rohit <rohitnarnolia88@gmail.com>
2026-08-18 20:07:54 +0530

    feat: add block_on in runtime

--- src/runtime.rs
+++ src/runtime.rs
@@ -3,7 +3,7 @@
 use std::{
     collections::BTreeMap,
     sync::{Arc, Mutex},
-    task::Waker,
+    task::{Context, Poll, Waker},
     time::Instant,
 };
 
@@ -59,7 +59,44 @@ impl Runtime {
         join_handle
     }
 
-    pub fn block_on<F: Future>(&self, _fut: F) -> F::Output {
-        todo!("wire up the executor")
+    pub fn block_on<F: Future>(&self, future: F) -> F::Output {
+        let waker = Waker::from(self.shared.awake_flag.clone());
+        let mut cx = Context::from_waker(&waker);
+        let mut main_task = Box::pin(future);
+        let mut other_tasks: Vec<DynFuture> = Vec::new();
+
+        loop {
+            self.shared.awake_flag.clear();
+
+            // whole runtime is done
+            if let Poll::Ready(output) = main_task.as_mut().poll(&mut cx) {
+                return output;
+            }
+
+            other_tasks.retain_mut(|task| task.as_mut().poll(&mut cx).is_pending());
+
+            loop {
+                let Some(mut task) = self.shared.new_tasks.lock().unwrap().pop() else {
+                    break;
+                };
+                if task.as_mut().poll(&mut cx).is_pending() {
+                    other_tasks.push(task);
+                }
+            }
+
+            if self.shared.awake_flag.is_set() {
+                continue;
+            }
+
+            let mut wake_times = self.shared.wake_times.lock().unwrap();
+            let next_wake = wake_times.keys().next().expect("kuch to gadbad h dya");
+            std::thread::sleep(next_wake.saturating_duration_since(Instant::now()));
+
+            while let Some(entry) = wake_times.first_entry()
+                && *entry.key() <= Instant::now()
+            {
+                entry.remove().into_iter().for_each(Waker::wake);
+            }
+        }
     }
 }
