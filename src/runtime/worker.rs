b48304ca9ddd84b8e715fc390c01113157ad229e
Rohit <rohitnarnolia88@gmail.com>
2026-08-21 20:26:09 +0530

    fix: pass thread local handler to run worker

--- src/runtime/worker.rs
+++ src/runtime/worker.rs
@@ -1,6 +1,8 @@
+use crate::runtime::Handle;
 use crate::task::TaskQueue;
 
-pub fn run_worker(queue: TaskQueue) {
+pub fn run_worker(handle: Handle, queue: TaskQueue) {
+    let _guard = handle.enter();
     loop {
         let task = queue.pop();
         task.poll()
